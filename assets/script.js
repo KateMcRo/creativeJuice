4cc929daefd49165e8b9c5da63de68910313b4dc
KateMcRo <katemcr0@gmail.com>
2023-03-19 10:52:07 -0700

    added test buttons and event listeners

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,16 @@
-// placeholder, JS Code goes here
+// Button Elements
+const colorSubmitBtnEl = document.getElementById("colorSubmit")
+const startOverBtnEl = document.getElementById("startOver")
+
+// Click Functions
+function colorTest () {
+    console.log("color")
+}
+
+function restartTest () {
+    console.log("Start Over")
+}
+
+// Event Listeners
+colorSubmitBtnEl.addEventListener("click", colorTest)
+startOverBtnEl.addEventListener("click", restartTest)
