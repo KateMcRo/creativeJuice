a0cfbcd93619c9f8f06a156e2c66c0353b1cab93
Paul Mendoza <60454231+pvmend@users.noreply.github.com>
2023-03-21 15:43:28 -0700

    Revert "added color picker array"

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,10 @@
 // Button Elements
 const wordSubmitBtnEl = document.getElementById("wordSubmit");
+const colorSubmitBtnEl = document.getElementById("colorSubmit");
 const startOverBtnEl = document.getElementById("startOver");
 
 // API Keys (key is the same for both APIs)
 const apiKey = "57fdf6d363msh1895db180f9cc69p1d283ejsna643fadf6b23"
-
-const colorPickerContainerEl = document.getElementById("pickerTest")
-const colorPickerArray = [
-    "#000000",
-    "#A6a6a6",
-    "#Ffffff",
-    "#F6F1EA",
-    "#C6B2A2",
-    "#62351B",
-    "#Ff3131",
-    "#Ff5757",
-    "#Ff66c4",
-    "#Cb6ce6",
-    "#8c52ff",
-    "#5e17eb",
-    "#0097b2",
-    "#0cc0df",
-    "#5ce1e6",
-    "#38b6ff",
-    "#5271ff",
-    "#004aad",
-    "#00bf63",
-    "#7ed957",
-    "#C1ff72",
-    "#Ffde59",
-    "#Ffbd59",
-    "#Ff914d",
-]
 
 // Input Elements
 let wordInputEl = document.getElementById("wordText"); 
@@ -40,23 +13,29 @@ let colorInputEl = document.getElementById("colorInput");
 const hexContainerEl = document.getElementById("hexTest");
 let colorObjectArray = []
 
+
+// Color Variable for parameter
+const options = {
+	method: 'POST',
+	headers: {
+		'content-type': 'application/json',
+		'X-RapidAPI-Key': apiKey,
+		'X-RapidAPI-Host': 'ai-color-generator.p.rapidapi.com'
+	},
+    // colorList array takes up to 4 strings of hex codes, ex: "#1e91c1"
+    // empty array generates totally random palette
+	body: '{"colorList":[]}'
+};
 // Color API call
-async function getColorData (hexCode) {
-    // Color Variable for parameter
-    const options = {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-            'X-RapidAPI-Key': apiKey,
-            'X-RapidAPI-Host': 'ai-color-generator.p.rapidapi.com'
-        },
-        // colorList array takes up to 4 strings of hex codes, ex: "#1e91c1"
-        body: `{"colorList":["${hexCode}"]}`
-    };
+async function getColorData () {
     const response = await fetch('https://ai-color-generator.p.rapidapi.com/generate-color', options)
     const data = await response.json()
     localStorage.setItem("data", JSON.stringify(data))
 }
+
+getColorData()
+handlePopulateColor()
+
 
 // API Data
 function handleColorObject () {
@@ -69,11 +48,11 @@ function handleColorObject () {
 function handlePopulateColor (){
     // returns parsed version of what was in local storage
     colorObject = handleColorObject()
-    hexContainerEl.innerHTML = ""
 
     for (let i = 0; i < colorObject.colorList.length; i++) {
     const hex = colorObject.colorList[i]
     const hexCard = generateCard(hex)
+    console.log(hexCard)
     hexContainerEl.append(hexCard)
     }
 }
@@ -93,34 +72,10 @@ function generateCard(hexCode){
     return colorContainer
 }
 
-// Color Picker for loop
-function handleColorPicker (){
-    // returns parsed version of what was in local storage
-    for (let i = 0; i < colorPickerArray.length; i++) {
-    const hex = colorPickerArray[i]
-    const pickHex = generateColorPicker(hex)
-    pickHex.addEventListener("click", (e)=> handleColorSubmit(e))
-    colorPickerContainerEl.appendChild(pickHex)
-    }
-}
-
-// Color Picker Test
-function generateColorPicker(hexCode) {
-    const pickerContainer = document.createElement("div")
-
-    pickerContainer.id = hexCode
-
-    pickerContainer.style.background = hexCode
-    pickerContainer.style.width = "2rem"
-    pickerContainer.style.height = "2rem"
-    return pickerContainer
-}
-
 // Click Functions
-async function handleColorSubmit (event) {
-    const hexCode = event.target.id
-    await getColorData(hexCode)
-    handlePopulateColor()
+function handleColorSubmit (event) {
+    event.preventDefault()
+    console.log("color")
 }
 
 // Word API call from user input
@@ -161,7 +116,7 @@ function showModule(element) {
 }
 
 // Event Listeners
+colorSubmitBtnEl.addEventListener("click", handleColorSubmit)
 startOverBtnEl.addEventListener("click", restartTest)
 wordSubmitBtnEl.addEventListener("click", renderSynonyms)
 
-handleColorPicker()
