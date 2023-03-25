274afb77e78020e26130f43156a2b3d765efd124
Dan <123746582+DanielFMcKay@users.noreply.github.com>
2023-03-24 21:19:46 -0700

    plz approve for now

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,10 @@ const wordSubmitBtnEl = document.getElementById("wordSubmit");
 const startOverBtnEl = document.getElementById("startOver");
 const startBtnEl = document.getElementById("startBtn");
 
+// container elements
+const contentGrid = document.getElementById("content-grid")
+const savedColorsEl = document.getElementById("savedColors")
+
 
 // Word Elements
 const wordCard1 = document.getElementById("word1");
@@ -11,6 +15,7 @@ const wordCard3 = document.getElementById("word3");
 const wordCard4 = document.getElementById("word4");
 const synonymGenEl = document.getElementById("wordForm")
 let synonymArray = [];
+let favoritesArray = []
 
 
 
@@ -25,6 +30,8 @@ const colorPickerArray = [
     "#F6F1EA",
     "#C6B2A2",
     "#62351B",
+    "#FFBD59",
+    "#FF914D",
     "#FF3131",
     "#FF5757",
     "#FF66C4",
@@ -41,17 +48,10 @@ const colorPickerArray = [
     "#7ED957",
     "#C1FF72",
     "#FFDE59",
-    "#FFBD59",
-    "#FF914D",
 ]
 
 // Color Elements
-const colorTilesAll = document.getElementsByClassName("colorTile")
-const colorTile0 = document.getElementById("color0");
-const colorTile1 = document.getElementById("color1");
-const colorTile2 = document.getElementById("color2");
-const colorTile3 = document.getElementById("color3");
-const colorTile4 = document.getElementById("color4");
+const colorTilesAll = document.querySelectorAll(".colorTile")
 
 let colorObjectArray = []
 
@@ -74,6 +74,7 @@ async function getColorData (hexCode) {
     const response = await fetch('https://ai-color-generator.p.rapidapi.com/generate-color', options)
     const data = await response.json()
     localStorage.setItem("data", JSON.stringify(data))
+    handleFavoriteColorDD()
 }
 
 // API Data
@@ -88,26 +89,17 @@ function handlePopulateTiles () {
     // returns parsed version of what was in local storage
     colorObject = handleColorObject()
     colorTilesAll.innerHTML = ""
+
+    for(let i = 0; i<colorObject.colorList.length; i += 1) {
+        const hex = colorObject.colorList[i]
+        const currentTile = colorTilesAll[i]
+        currentTile.innerHTML = `<div id="parent-${hex}" style="display:flex; align-items:center;"> <i id="icon-${hex}" class="fa fa-thumbtack favorite-icon" style="color:white; margin-right: 1rem;"></i>${hex}</div>`
+        currentTile.style.background = hex
+        currentTile.setAttribute("id", hex)
+    }
+
+    handleFavoriteColorDD()
     
-    const hex0 = colorObject.colorList[0]
-    colorTile0.style.background = hex0
-    colorTile0.innerText = hex0
-
-    const hex1 = colorObject.colorList[1]
-    colorTile1.style.background = hex1
-    colorTile1.innerText = hex1
-
-    const hex2 = colorObject.colorList[2]
-    colorTile2.style.background = hex2
-    colorTile2.innerText = hex2
-
-    const hex3 = colorObject.colorList[3]
-    colorTile3.style.background = hex3
-    colorTile3.innerText = hex3
-
-    const hex4 = colorObject.colorList[4]
-    colorTile4.style.background = hex4
-    colorTile4.innerText = hex4
 }
 
 // Adds event listeners to each color square
@@ -117,19 +109,19 @@ function handleColorPicker (){
     //colorPickerContainerEl.style.flexDirection = "row"
     //colorPickerContainerEl.style.alignContent = "space-around"
     // Above styling can be done in CSS
-
+    
     for (let i = 0; i < colorPickerArray.length; i++) {
-    const hex = colorPickerArray[i]
-    const pickHex = generateColorPicker(hex)
-    pickHex.addEventListener("click", (e)=> handleColorSubmit(e))
-    colorPickerContainerEl.appendChild(pickHex)
+        const hex = colorPickerArray[i]
+        const pickHex = generateColorPicker(hex)
+        pickHex.addEventListener("click", (e)=> handleColorSubmit(e))
+        colorPickerContainerEl.appendChild(pickHex)
     }
 }
 
 // Creates Color Picker Squares in container
 function generateColorPicker(hexCode) {
     const pickerContainer = document.createElement("div")
-
+    
     pickerContainer.id = hexCode
     pickerContainer.className = "pickBox"
     pickerContainer.style.background = hexCode
@@ -145,105 +137,106 @@ async function handleColorSubmit (event) {
     const hexCode = event.target.id
     await getColorData(hexCode)
     handlePopulateTiles()
-
+    
 }
-    var modal = document.getElementById("myModal");
+var modal = document.getElementById("myModal");
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("wordSubmit");
+// Get the button that opens the modal
+var btn = document.getElementById("wordSubmit");
 
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
 
-    // When the user clicks the button, open the modal 
-    // btn.onclick = function() {
+
+
+// When the user clicks the button, open the modal 
+// btn.onclick = function() {
     // modal.style.display = "block";
     // }
-
+    
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
         modal.style.display = "none";
-        }
-
+    }
+    
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
     }
-    }
-  
-
-
-// Word API call from user input
-function renderSynonyms(){
-	let wordValueEl = document.querySelector('#wordText').value;
-	
-	const getSynonyms = {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Key': apiKey ,
-			'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
+    
+    
+    
+    // Word API call from user input
+    function renderSynonyms(){
+        let wordValueEl = document.querySelector('#wordText').value;
+        
+        const getSynonyms = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': apiKey ,
+                'X-RapidAPI-Host': 'wordsapiv1.p.rapidapi.com'
 			}
-	};
-
-	fetch(`https://wordsapiv1.p.rapidapi.com/words/${wordValueEl}/synonyms`, getSynonyms)
-	.then(function(response){
-        console.log(response)
-        if(response.status !== 200){
-            modal.style.display = "block";
-        } else{
-            return response.json();
-        }
-		
-	})
-	.then(function(data){
-        if(!data || !data.synonyms){
-            return;
-        }
-        if (data.synonyms.length === 0 ){
-            
-            // display modal
-           
+        };
+        
+        fetch(`https://wordsapiv1.p.rapidapi.com/words/${wordValueEl}/synonyms`, getSynonyms)
+        .then(function(response){
+            console.log(response)
+            if(response.status !== 200){
+                modal.style.display = "block";
+            } else{
+                return response.json();
             }
-		console.log(data);
-		console.log(data.synonyms);
-	
-	//displayWord.innerText = wordValueEl;
-    let searchedWord = document.getElementById('searchedWord');
-    searchedWord.innerText = wordValueEl;
-	
-	synonymArray = [];
-	for(let i = 0; i < data.synonyms.length; i++){
-		
-		//console.log(data.synonyms[i])
-		synonymArray.push(data.synonyms[i]);
-	}
-    // if(synonymArray < 4){
-    //     fourSynonyms.push(synonymArray)
-    // }
-	console.log(synonymArray)
-    let fourSynonyms = [];
-    for (let i = 0; i < 4; i++){
-        console.log(i)
-        console.log(synonymArray[i])
-        if(synonymArray[i] !== undefined){
-         
-            var randomWord = synonymArray[Math.floor(Math.random() * synonymArray.length)];
-          //  console.log(!fourSynonyms.includes(randomWord))
-            if(!fourSynonyms.includes(randomWord)){
-               // console.log(true)  
-              //  console.log(randomWord)
-               // console.log(fourSynonyms);
-                fourSynonyms.push(randomWord);
+            
+        })
+        .then(function(data){
+            if(!data || !data.synonyms){
+                return;
+            }
+            if (data.synonyms.length === 0 ){
+                
+                // display modal
                 
             }
-        }
-        
-        // first call a for loop using math.floor(math.random () * array.length)
-        // use an if statement to check for duplicates  
+            console.log(data);
+            console.log(data.synonyms);
+            
+            //displayWord.innerText = wordValueEl;
+            let searchedWord = document.getElementById('searchedWord');
+            searchedWord.innerText = wordValueEl;
+            
+            synonymArray = [];
+            for(let i = 0; i < data.synonyms.length; i++){
+                
+                //console.log(data.synonyms[i])
+                synonymArray.push(data.synonyms[i]);
+            }
+            // if(synonymArray < 4){
+                //     fourSynonyms.push(synonymArray)
+                // }
+                console.log(synonymArray)
+                let fourSynonyms = [];
+                for (let i = 0; i < 4; i++){
+                    console.log(i)
+                    console.log(synonymArray[i])
+                    if(synonymArray[i] !== undefined){
+                        
+                        var randomWord = synonymArray[Math.floor(Math.random() * synonymArray.length)];
+                        //  console.log(!fourSynonyms.includes(randomWord))
+                        if(!fourSynonyms.includes(randomWord)){
+                            // console.log(true)  
+                            //  console.log(randomWord)
+                            // console.log(fourSynonyms);
+                            fourSynonyms.push(randomWord);
+                            
+                        }
+                      
+                    }
+                    
+                    // first call a for loop using math.floor(math.random () * array.length)
+                    // use an if statement to check for duplicates  
         // push elements into new array
 
         //console.log(fourSynonyms);
@@ -295,6 +288,7 @@ function hideResults(){
     landingContainer.style.display = "flex";
 }
 function hideLanding(){
+    handleFavoriteColorDD()
     let landingContainer = document.getElementById("landingContainer");
     landingContainer.style.display = "none";
     let inputArea = document.getElementById("inputArea");
@@ -305,12 +299,62 @@ function hideLanding(){
     resultsPage.style.display = "flex";
     
 }
+
+function handleFavorites(e){
+    
+    if (e.target.tagName !== "I") {
+        return null
+    }
+    const id = e.target.id
+    const clickedFavorite = document.getElementById(id)
+    favoritesArray = []
+    const prevFavorites = JSON.parse(localStorage.getItem("favorites"))
+    
+    prevFavorites?.forEach((item) => {
+        favoritesArray.push(item)
+    })
+    const favorited = prevFavorites?.find((item) => item === id)
+    if (favorited) {
+        clickedFavorite.setAttribute("style", "color: white; margin-right: 1rem;")
+        favoritesArray = favoritesArray.filter((item) => item !== id)
+        localStorage.setItem("favorites", JSON.stringify(favoritesArray))
+    } else if (!favorited) {
+        clickedFavorite.setAttribute("style", "color: black; margin-right: 1rem;")
+        favoritesArray.push(id)
+        localStorage.setItem("favorites", JSON.stringify(favoritesArray))
+    }
+
+    handleFavoriteColorDD()
+
+}
+
+function handleFavoriteColorDD () {
+    favoritesArray = []
+    const prevFavorites = JSON.parse(localStorage.getItem("favorites"))
+    prevFavorites?.forEach((item) => {
+        favoritesArray.push(item)
+    })
+    savedColorsEl.innerHTML = ""
+    favoritesArray.forEach((item) => {
+        const icon = document.getElementById(`${item}`)
+        if (icon) {
+            icon.setAttribute("style", "color: black; margin-right: 1rem;")
+        }
+        const cell = document.createElement("div")
+        const hex = item.split("-")[1]
+        cell.setAttribute("style", `background: ${hex}; color: black;`)
+        cell.innerText = hex
+        savedColorsEl.append(cell)
+    })
+}
+
 hideResults();
 // Event Listeners
 startOverBtnEl.addEventListener("click", restartTest);
 wordSubmitBtnEl.addEventListener("click", renderSynonyms);
 startBtnEl.addEventListener("click", hideLanding);
 startOverBtnEl.addEventListener('click', hideResults);
+contentGrid.addEventListener("click", (e) => handleFavorites(e))
+
 // this will need to be moved to the end of the word input
 handleColorPicker();
-
